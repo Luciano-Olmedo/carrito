6d5f48e1e1ec4ad8fcf14ca41449e2d9fec869a7
Luciano-Olmedo <lucianonicolasolmedo@gmail.com>
2024-05-23 21:21:10 -0300

    Cart Provider

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,23 @@ import { Routes, Route, Navigate } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import ComprasPage from './pages/ComprasPage'
 import Carrito from './pages/Carrito'
-import {ProductosProvider} from './context/ProductosProvider'
+import { ProductosProvider } from './context/ProductosProvider'
+import { CartProvider } from './context/CartProvider'
 
 
 export const App = () => {
   return (
     <ProductosProvider>
-      <Navbar />
-      <div className='container'>
-        <Routes>
-          <Route path='/' element={<ComprasPage />}>    </Route>
-          <Route path='/carrito' element={<Carrito />}>    </Route>
-          <Route path='/*' element={<Navigate to="/" />}>  </Route>
-        </Routes>
-      </div>
+      <CartProvider>
+        <Navbar />
+        <div className='container'>
+          <Routes>
+            <Route path='/' element={<ComprasPage />}>    </Route>
+            <Route path='/carrito' element={<Carrito />}>    </Route>
+            <Route path='/*' element={<Navigate to="/" />}>  </Route>
+          </Routes>
+        </div>
+      </CartProvider>
     </ProductosProvider>
   )
 }
--- src/context/CartProvider.jsx
+++ src/context/CartProvider.jsx
@@ -1,17 +1,69 @@
-import {CartContext} from './CartContext'
+import { CartContext } from './CartContext'
 import { useReducer } from 'react'
 
 const initialState = []
 
-export const CartProvider = ({Children}) => {
+export const CartProvider = ({ Children }) => {
 
-    const [listaCompras, dispatch] = useReducer(comprasReducer,initialState)
+  const [listaCompras, dispatch] = useReducer(comprasReducer, initialState)
+
+  const agregarCompra = (compra) => {
+    const action = {
+      type: '[CART] Agregar Compra',
+      payload: compra
+    }
+    dispatch(action)
+  }
+  const aumentarCantidad = (id) => {
+    const action = {
+      type: '[CART] Aumentar Cantidad Compra',
+      payload: id
+    }
+    dispatch(action)
+  }
+  const disminuirCantidad = (id) => {
+    const action = {
+      type: '[CART] disminuir Cantidad Compra',
+      payload: id
+    }
+    dispatch(action)
+  }
+  const eliminarCompra = (id) => {
+    const action = {
+      type: '[CART] Eliminar Compra',
+      payload: id
+    }
+    dispatch(action)
+  }
+
+  const comprasReducer = (state = initialState, action = {}) => {
+    switch (action.type) {
+      case '[CART] Agregar Compra':
+        return [...state, action.payload]
+
+      case '[CART] Aumentar Cantidad Compra':
+
+        break;
+
+      case '[CART] Agregar Compra':
+
+        break;
+
+      case '[CART] Eliminar Compra':
+
+        return state.filter(compra => compra.id !== action.payload)
+
+
+      default:
+        return state;
+    }
+  }
 
 
   return (
-    <CartProvider.CartProvider>
-        {Children}
-    </CartProvider.CartProvider>   
+    <CartProvider.CartProvider value={{ listaCompras, agregarCompra, aumentarCantidad, disminuirCantidad, eliminarCompra }}>
+      {Children}
+    </CartProvider.CartProvider>
   )
 }
 
