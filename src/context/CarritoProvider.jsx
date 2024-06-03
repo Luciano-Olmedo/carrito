32988b36a1498c75c3fc84fb4ade2255e0672e4a
Luciano-Olmedo <lucianonicolasolmedo@gmail.com>
2024-06-03 20:53:48 -0300

    reducer

--- src/context/CarritoProvider.jsx
+++ src/context/CarritoProvider.jsx
@@ -1,9 +1,40 @@
-import { Children } from "react"
+import { useReducer } from "react"
 import { CarritoContext } from "./CarritoContext"
 
 const initialState = []
 
 export const CarritoProvider = ({ Children }) => {
+
+    const [listaCompras, dispatch] = useReducer(comprasReducer, initialState)
+
+    const agregarCompra = (compra) => {
+        const action = {
+            type: '',
+            payload: compra
+        }
+        dispatch(action)
+    }
+    const aumentarCantidad = (id) => {
+        const action = {
+            type: '',
+            payload: id
+        }
+        dispatch(action)
+    }
+    const disminuirCantidad = (id) => {
+        const action = {
+            type: '',
+            payload: id
+        }
+        dispatch(action)
+    }
+    const eliminarCompra = (id) => {
+        const action = {
+            type: '',
+            payload: id
+        }
+        dispatch(action)
+    }
 
     return (
         <CarritoContext>
