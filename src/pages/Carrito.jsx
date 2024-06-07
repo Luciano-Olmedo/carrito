b01a0f1c976be22128567dc2ca39d1f96461bbb8
Luciano-Olmedo <lucianonicolasolmedo@gmail.com>
2024-06-07 20:44:06 -0300

    react icons and animation cart

--- src/pages/Carrito.jsx
+++ src/pages/Carrito.jsx
@@ -1,5 +1,7 @@
 import { useContext } from "react"
 import { CarritoContext } from "../context/CarritoContext"
+import { FaRegTrashCan } from "react-icons/fa6";
+
 
 
 
@@ -9,25 +11,25 @@ const Carrito = () => {
 
   return (
     <>
-      <div className="container mx-auto">
-        <table className="min-w-full">
+      <div className="container lg:mx-auto  lg:p-2">
+        <table className="min-w-full ">
           <thead className="bg-gray-800 text-white">
             <tr>
               <th className="py-2 px-4">Título</th>
-              <th className="py-2 px-4">Descripción</th>
               <th className="py-2 px-4">Precio</th>
+              <th className="py-2 px-4">Cantidad</th>
               <th className="py-2 px-4">Eliminar</th>
             </tr>
           </thead>
-          <tbody className="bg-gray-100">
+          <tbody className="bg-gray-100 ">
             {
               listaCompras.map(item => (
                 <tr key={item.id}>
-                  <td className="border px-4 py-2">{item.title}</td>
-                  <td className="border px-4 py-2">{item.price}</td>
-                  <td className="border px-4 py-2 text-center">1</td>
-                  <td className="border px-4 py-2 text-center">
-                    <button type="button" className="" onClick={(id) => eliminarCompra(item.id)}>Eliminar</button>
+                  <td className="border px-4 py-2 font-bold">{item.title}</td>
+                  <td className="border px-4 py-2 text-center font-bold">${item.price}</td>
+                  <td className="border px-4 py-2 text-center font-bold">1</td>
+                  <td className="border px-4 py-2 text-center font-bold">
+                    <button type="button" className="" onClick={(id) => eliminarCompra(item.id)}><FaRegTrashCan className="text-red-400"/></button>
                   </td>
                 </tr>
               ))
