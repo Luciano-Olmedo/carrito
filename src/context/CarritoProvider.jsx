03402ff6c0069f9c7006733ecc3271ff4ac74860
Luciano-Olmedo <lucianonicolasolmedo@gmail.com>
2024-06-09 20:03:29 -0300

    billing and print

--- src/context/CarritoProvider.jsx
+++ src/context/CarritoProvider.jsx
@@ -65,7 +65,7 @@ export const CarritoProvider = ({ children }) => {
         dispatch(action)
 
     }
-
+    
 
 
 
--- src/pages/Carrito.jsx
+++ src/pages/Carrito.jsx
@@ -9,6 +9,13 @@ import { FaRegTrashCan } from "react-icons/fa6";
 const Carrito = () => {
   const { listaCompras, aumentarCantidad, disminuirCantidad, eliminarCompra } = useContext(CarritoContext)
 
+  const calcularTotal = () => {
+    return listaCompras.reduce((total, item) => total + item.price * item.cantidad, 0).toFixed(2)
+  }
+  const handleImpresion = () => {
+    print()
+  }
+
   return (
     <>
       <div className="container lg:mx-auto  lg:p-2">
@@ -28,23 +35,38 @@ const Carrito = () => {
                   <td className="border px-4 py-2 font-bold">{item.title}</td>
                   <td className="border px-4 py-2 text-center font-bold">${item.price}</td>
                   <td className="border px-4 py-2 text-center font-bold">
-                    <button onClick={() => disminuirCantidad(item.cantidad)} className="px-4 py-2 border font-bold lg:mr-2 bg-blue-300 hover:scale-125 rounded-lg">menos</button>
+                    <button
+                      onClick={() => disminuirCantidad(item.cantidad)}
+                      className="px-4 py-2 border font-bold lg:mr-2 bg-blue-300 hover:scale-125 rounded-lg">-
+                    </button>
                     <span className="px-4 py-2 border bg-white">{item.cantidad}</span>
-                    <button onClick={() => aumentarCantidad(item.cantidad)} className="px-4 py-2 border font-bold lg:ml-2 bg-blue-300 hover:scale-125 rounded-lg">mas</button>
-
-
+                    <button
+                      onClick={() => aumentarCantidad(item.cantidad)}
+                      className="px-4 py-2 border font-bold lg:ml-2 bg-blue-300 hover:scale-125 rounded-lg">+
+                    </button>
                   </td>
                   <td className="border px-4 py-2 text-center font-bold">
-                    <button type="button" className="" onClick={(id) => eliminarCompra(item.id)}><FaRegTrashCan className="text-red-400" /></button>
+                    <button
+                      type="button"
+                      onClick={(id) => eliminarCompra(item.id)}><FaRegTrashCan className="text-red-400" />
+                    </button>
                   </td>
                 </tr>
               ))
             }
+            <th className="">TOTAL:</th>
+            <td></td>
+            <td></td>
+            <td className="bg-blue-300 font-bold text-center">${calcularTotal()}</td>
           </tbody>
         </table>
 
       </div>
-      <button className="mx-auto focus:outline-none  text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 lg:mt-4 lg:ml-2">Comprar</button>
+      <button
+        onClick={handleImpresion}
+        disabled={listaCompras < 1}
+        className="mx-auto focus:outline-none  text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 lg:mt-4 lg:ml-2">Comprar
+      </button>
 
 
     </>
